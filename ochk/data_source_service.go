1b54faacce4e2e4ad86a30f6835cb94b35b8916f
Maciej Karaś <6159874+MaciejKaras@users.noreply.github.com>
2020-08-04 13:35:38 +0200

    Added dataSource for DefaultServices (#5)

---
+++ ochk/data_source_service.go
@@ -0,0 +1,45 @@
+package ochk
+
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/ochk/terraform-provider-ochk/ochk/sdk"
+)
+
+//TODO brak testu akceptacyjnego
+func dataSourceService() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: datSourceServiceRead,
+
+		Schema: map[string]*schema.Schema{
+			"display_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func datSourceServiceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	proxy := meta.(*sdk.Client).Services
+
+	displayName := d.Get("display_name").(string)
+
+	services, err := proxy.ListByDisplayName(ctx, displayName)
+	if err != nil {
+		return diag.Errorf("error while listing services: %+v", err)
+	}
+
+	if len(services) < 1 {
+		return diag.Errorf("no service found for display_name: %s", displayName)
+	}
+
+	if len(services) > 1 {
+		return diag.Errorf("more than one service with display_name: %s found!", displayName)
+	}
+
+	d.SetId(services[0].ServiceID)
+
+	return nil
+}
