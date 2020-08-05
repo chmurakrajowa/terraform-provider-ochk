51d92284c1a82d8c6ce8dd7034c3a50807c782d4
Maciej Karaś <6159874+MaciejKaras@users.noreply.github.com>
2020-08-05 15:07:28 +0200

    Adding missing acc test for data sources (#8)

---
+++ ochk/data_source_service_test.go
@@ -0,0 +1,31 @@
+package ochk
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+func TestAccServiceDataSource_read(t *testing.T) {
+	resourceName := "data.ochk_service.http"
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccServiceDataSourceConfig("http"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "display_name", "http"),
+				),
+			},
+		},
+	})
+}
+
+func testAccServiceDataSourceConfig(displayName string) string {
+	return fmt.Sprintf(`
+data "ochk_service" "http" {
+  display_name = %[1]q
+}
+`, displayName)
+}
