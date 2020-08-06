c18b35d33a726fe536a2810c186fed5489e82025
Łukasz Sierant <lukasz.sierant@ochk.pl>
2020-08-06 12:28:13 +0200

    Example resource configuration

---
+++ examples/terraform-services.tf
@@ -0,0 +1,7 @@
+data "ochk_service" "http" {
+  display_name = "http"
+}
+
+data "ochk_service" "ssh" {
+  display_name = "ssh"
+}
---
+++ examples/terraform-variables.tf
@@ -0,0 +1,15 @@
+variable "demo-id" {
+  default = "demo"
+}
+
+variable "host" {
+}
+
+variable "tenant" {
+}
+
+variable "username" {
+}
+
+variable "debug_log_file" {
+}
