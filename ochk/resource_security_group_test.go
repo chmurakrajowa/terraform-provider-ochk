ec9c9176678fd23680d22c73ba76c22329b778f7
Maciej Karaś <6159874+MaciejKaras@users.noreply.github.com>
2020-08-19 14:36:08 +0200

    Security Group update (#17)

--- ochk/resource_security_group_test.go
+++ ochk/resource_security_group_test.go
@@ -9,25 +9,105 @@ import (
 	"testing"
 )
 
-func TestAccSecurityGroupResource_create(t *testing.T) {
-	resourceName := "ochk_security_group.one_member"
-	displayName := generateRandName()
+type SecurityGroupTestData struct {
+	ResourceName string
+	DisplayName  string
+	Members      []SecurityGroupMemberTestData
+}
 
-	//TODO zbyt wiele razy jest wołany POST /vidm/token HTTP/1.1, coś jest nie tak
+type SecurityGroupMemberTestData struct {
+	ID   string
+	Type string
+}
+
+func (c *SecurityGroupTestData) ToString() string {
+	return executeTemplateToString(`
+resource "ochk_security_group" "{{ .ResourceName}}" {
+  display_name = "{{ .DisplayName}}"
+
+  {{range $member := .Members}}
+  members {
+    id   = {{ $member.ID }}
+    type = "{{ $member.Type }}"
+  }
+  {{end}}
+}
+`, c)
+}
+
+func (c *SecurityGroupTestData) FullResourceName() string {
+	return "ochk_security_group." + c.ResourceName
+}
+
+func TestAccSecurityGroupResource_create(t *testing.T) {
+	/* Security group with one member */
+	virtualMachine := VirtualMachineDataSourceTestData{
+		ResourceName: "default",
+		DisplayName:  testDataVirtualMachine1DisplayName,
+	}
+
+	securityGroup := SecurityGroupTestData{
+		ResourceName: "one_member",
+		DisplayName:  generateRandName(),
+		Members: []SecurityGroupMemberTestData{
+			{
+				ID:   virtualMachine.FullResourceName() + ".id",
+				Type: "VIRTUAL_MACHINE",
+			},
+		},
+	}
+	configOneMember := virtualMachine.ToString() + securityGroup.ToString()
+
+	/* Security group with one member with updated display_name */
+	securityGroupUpdated := securityGroup
+	securityGroupUpdated.DisplayName += "-updated"
+	configOneMemberUpdated := virtualMachine.ToString() + securityGroupUpdated.ToString()
+
+	/* Security group with two members and updated display_name */
+	securityGroupTwoMembers := securityGroupUpdated
+	virtualMachine2 := VirtualMachineDataSourceTestData{
+		ResourceName: "default2",
+		DisplayName:  testDataVirtualMachine1DisplayName,
+	}
+	securityGroupTwoMembers.Members = append(securityGroupTwoMembers.Members, SecurityGroupMemberTestData{
+		ID:   virtualMachine2.FullResourceName() + ".id",
+		Type: "VIRTUAL_MACHINE",
+	})
+	configTwoMembers := securityGroupTwoMembers.ToString() + virtualMachine.ToString() + virtualMachine2.ToString()
+
+	securityGroupResourceName := securityGroup.FullResourceName()
 	resource.Test(t, resource.TestCase{
 		ProviderFactories: testAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSecurityGroupResourceConfig(displayName),
+				Config: configOneMember,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "display_name", displayName),
-					resource.TestCheckResourceAttrPair(resourceName, "members.0.id", "data.ochk_virtual_machine.default", "id"),
-					resource.TestCheckResourceAttr(resourceName, "members.0.type", "VIRTUAL_MACHINE"),
-					resource.TestCheckResourceAttrSet(resourceName, "members.0.display_name"),
+					resource.TestCheckResourceAttr(securityGroupResourceName, "display_name", securityGroup.DisplayName),
+					resource.TestCheckResourceAttrPair(securityGroupResourceName, "members.0.id", virtualMachine.FullResourceName(), "id"),
+					resource.TestCheckResourceAttr(securityGroupResourceName, "members.0.type", securityGroup.Members[0].Type),
+					resource.TestCheckResourceAttrSet(securityGroupResourceName, "members.0.display_name"),
+				),
+			},
+			{
+				Config: configOneMemberUpdated,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(securityGroupResourceName, "display_name", securityGroupUpdated.DisplayName),
+				),
+			},
+			{
+				Config: configTwoMembers,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(securityGroupResourceName, "display_name", securityGroupTwoMembers.DisplayName),
+					resource.TestCheckResourceAttrPair(securityGroupResourceName, "members.0.id", virtualMachine.FullResourceName(), "id"),
+					resource.TestCheckResourceAttr(securityGroupResourceName, "members.0.type", securityGroupTwoMembers.Members[0].Type),
+					resource.TestCheckResourceAttrSet(securityGroupResourceName, "members.0.display_name"),
+					resource.TestCheckResourceAttrPair(securityGroupResourceName, "members.1.id", virtualMachine2.FullResourceName(), "id"),
+					resource.TestCheckResourceAttr(securityGroupResourceName, "members.1.type", securityGroupTwoMembers.Members[1].Type),
+					resource.TestCheckResourceAttrSet(securityGroupResourceName, "members.1.display_name"),
 				),
 			},
 		},
-		CheckDestroy: testAccSecurityGroupResourceNotExists(displayName),
+		CheckDestroy: testAccSecurityGroupResourceNotExists(securityGroupTwoMembers.DisplayName),
 	})
 }
 
@@ -46,21 +126,4 @@ func testAccSecurityGroupResourceNotExists(displayName string) resource.TestChec
 
 		return nil
 	}
-}
-
-func testAccSecurityGroupResourceConfig(name string) string {
-	return fmt.Sprintf(`
-data "ochk_virtual_machine" "default" {
-  display_name = %[2]q
-}
-
-resource "ochk_security_group" "one_member" {
-  display_name = %[1]q
-
-  members {
-    id = data.ochk_virtual_machine.default.id
-    type = "VIRTUAL_MACHINE"
-  }
-}
-`, name, testDataVirtualMachine1DisplayName)
 }
--- ochk/testdata_test.go
+++ ochk/testdata_test.go
@@ -1,6 +1,25 @@
 package ochk
 
+import (
+	"bytes"
+	"text/template"
+)
+
 const (
 	testDataVirtualMachine1DisplayName = "devel0000001157"
 	testDataIPSet1DisplayName          = "googledns"
 )
+
+func executeTemplateToString(templateString string, data interface{}) string {
+	parsedTemplate, err := template.New("template_name").Parse(templateString)
+	if err != nil {
+		panic(err)
+	}
+
+	var tpl bytes.Buffer
+	if err := parsedTemplate.Execute(&tpl, data); err != nil {
+		panic(err)
+	}
+
+	return tpl.String()
+}
