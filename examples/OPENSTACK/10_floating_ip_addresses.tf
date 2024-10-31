7cc8fc80b2d1976ab61a6138f090d6819e431eef
damianstawicki <damian.stawicki@ochk.pl>
2024-10-31 15:17:14 +0100

    TODO

--- examples/OPENSTACK/10_floating_ip_addresses.tf
+++ examples/OPENSTACK/10_floating_ip_addresses.tf
@@ -9,6 +9,6 @@ data "ochk_floating_ip_address" "default" {
 resource "ochk_floating_ip_address" "tj1" {
     display_name = "XXXXXXXX"
     description = "XXXXXXXX"
-    vm_port_id = "d3c6bda6-172d-408b-a3ce-4f025f467e31"
+    vm_port_id = "d3c6bda6-172d-408b-a3ce-4f025f467e31" // TODO dodać endpoint do proxy aby można było pobrać porty vm przez providera
 }
 
