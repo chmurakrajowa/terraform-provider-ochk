8f67d58b504467ddc3ba14a13cdc2cdf01a05f9e
damianstawicki <damian.stawicki@ochk.pl>
2024-11-29 12:03:05 +0100

    Add getPlatformType () to project_test.go

--- ochk/resource_project_test.go
+++ ochk/resource_project_test.go
@@ -3,6 +3,7 @@ package ochk
 import (
 	"context"
 	"fmt"
+	"github.com/chmurakrajowa/terraform-provider-ochk/ochk/api/v3/models"
 	"github.com/chmurakrajowa/terraform-provider-ochk/ochk/sdk"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
@@ -24,6 +25,15 @@ type ProjectTestData struct {
 
 func checkPlatformType() string {
 	return os.Getenv("TF_VAR_platform_type")
+}
+
+func getPlatformType() models.PlatformType {
+	platformType := os.Getenv("TF_VAR_platform_type")
+	if platformType == "OPENSTACK" {
+		return models.PlatformTypeOPENSTACK
+	} else {
+		return models.PlatformTypeVMWARE
+	}
 }
 
 func (c *ProjectTestData) ToString() string {
