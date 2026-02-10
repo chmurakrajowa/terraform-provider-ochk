ddef88894bc8281bc9d11e2b89abfe35f54e2778
damianstawicki <damian.stawicki@ochk.pl>
2026-02-10 16:13:46 +0100

    Get swagger.yaml from server

--- tools/swagger_update.sh
+++ tools/swagger_update.sh
@@ -4,8 +4,14 @@ set -eo pipefail
 
 output_file=ochk/api/swagger.json
 output_file2=ochk/api/swagger2.json
+output_file_yaml=ochk/api/swagger.yaml
+
 TF_VAR_host=pckproxy.ochk.pilot
 echo "url:"${TF_VAR_host}
-curl -s https://${TF_VAR_host}/swagger/v1/swagger.json | jq '.' >${output_file}
-curl -s https://${TF_VAR_host}/swagger/v1/swagger.json  >  ${output_file2}
+curl -s -v https://${TF_VAR_host}/swagger/v1/swagger.json | jq '.' >${output_file}
+curl -s -v https://${TF_VAR_host}/swagger/v1/swagger.json  >  ${output_file2}
+curl -s -v https://${TF_VAR_host}/swagger/v1/swagger.yaml  >  ${output_file_yaml}
+#curl -s -v http://localhost:8060/swagger/v1/swagger.json | jq '.' >${output_file}
+#curl -s -v http://localhost:8060/swagger/v1/swagger.json  >  ${output_file2}
 echo "Updated swagger file: ${output_file}"
+echo "Updated swagger yaml file: ${output_file_yaml}"
