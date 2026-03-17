d75292ed5bb965807c4bf292a5773242c9225e3b
damianstawicki <damian.stawicki@ochk.pl>
2026-03-17 14:53:48 +0100

    Fix for deployment list

--- ochk/structures_deployment.go
+++ ochk/structures_deployment.go
@@ -15,11 +15,11 @@ func flattenDeployments(in []openapi.DeploymentInstance, deploymentType openapi.
 	for _, v := range in {
 		if deploymentType == "" || deploymentType == *v.DeploymentType {
 			m := make(map[strfmt.UUID]interface{})
-			m["deployment_id"] = v.DeploymentId
-			m["display_name"] = v.DisplayName
-			m["deployment_type"] = v.DeploymentType
-			m["deployment_category"] = v.DeploymentCategory
-			m["initial_size_gb"] = v.DeploymentInitialSizeGB
+			m["deployment_id"] = v.GetDeploymentId()
+			m["display_name"] = v.GetDisplayName()
+			m["deployment_type"] = v.GetDeploymentType()
+			m["deployment_category"] = v.GetDeploymentCategory()
+			m["initial_size_gb"] = v.GetDeploymentInitialSizeGB()
 			out = append(out, m)
 		}
 	}
