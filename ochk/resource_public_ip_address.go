19b89bc933281d9ddb02c35d8e906b028fa8b846
licjus <71689899+licjus@users.noreply.github.com>
2026-03-31 11:46:59 +0200

    Public ip address

---
+++ ochk/resource_public_ip_address.go
@@ -0,0 +1,108 @@
+package ochk
+
+import (
+	"context"
+	"github.com/chmurakrajowa/terraform-provider-ochk/ochk/api/v3"
+	"github.com/chmurakrajowa/terraform-provider-ochk/ochk/sdk"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+const (
+	PublicIpRetryTimeout = 5 * time.Minute
+)
+
+func resourcePublicIp() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourcePublicIpCreate,
+		ReadContext:   resourcePublicIpRead,
+		UpdateContext: resourcePublicIpUpdate,
+		DeleteContext: resourcePublicIpDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(PublicIpRetryTimeout),
+			Update: schema.DefaultTimeout(PublicIpRetryTimeout),
+			Delete: schema.DefaultTimeout(PublicIpRetryTimeout),
+		},
+
+		Importer: &schema.ResourceImporter{
+			StateContext: resourcePublicIpImportState,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"public_ip_address_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ip_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourcePublicIpImportState(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
+	d.SetId(strings.ToLower(d.Id()))
+	return []*schema.ResourceData{d}, nil
+}
+
+func resourcePublicIpCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	proxy := meta.(*sdk.Client).PublicIPAddresses
+	proxy_pt := meta.(*sdk.Client).PlatformType
+	platformType, _ := proxy_pt.Read(ctx)
+
+	public_ip_address := mapResourceDataToPublicIp(d, platformType)
+
+	created, err := proxy.Create(ctx, public_ip_address, 1000)
+	if err != nil {
+		return diag.Errorf("error while creating public ip address: %+v", err)
+	}
+
+	d.SetId(created.GetRequestId())
+	return resourcePublicIpRead(ctx, d, meta)
+}
+
+func resourcePublicIpRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
+	return nil
+}
+
+func mapPublicIpToResourceData(d *schema.ResourceData, project openapi.ProjectInstance) error {
+
+	return nil
+}
+
+func resourcePublicIpUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+}
+
+func resourcePublicIpDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	proxy := meta.(*sdk.Client).PublicIPAddresses
+
+	err := proxy.Delete(ctx, d.Id()) //Delete(ctx, strfmt.UUID(d.Id()))
+	if err != nil {
+		if sdk.IsNotFoundError(err) {
+			id := d.Id()
+			d.SetId("")
+			return diag.Errorf("public ip address with id %s not found: %+v", id, err)
+		}
+
+		return diag.Errorf("error while deleting public ip address: %+v", err)
+	}
+
+	return nil
+}
+
+func mapResourceDataToPublicIp(d *schema.ResourceData, platformType openapi.PlatformType) openapi.PublicIpAllocation {
+	return openapi.PublicIpAllocation{
+		//PublicIpAddress:   d.Get("ip_address").(string),
+		Name: NewNullableString(d.Get("display_name").(string)),
+	}
+}
